912605543dd37808b0f703ed8804659bf19d91ca
dyerman456 <dyerman456@gmail.com>
2025-06-05 17:15:47 +0200

    change css and App

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 import {OnOff} from "./components/Onoff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 function App() {
     return (
         <div className="App">
             <OnOff />
             <UncontrolledAccordion titleValue={"Menu"}/>
-            <UncontrolledAccordion titleValue={"Users"}/>
+            <UncontrolledRating/>
+
             {/*<PageTitle title="This is App component"/>*/}
             {/*<PageTitle title="My friends"/>*/}
             {/*Article 1*/}
